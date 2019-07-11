78657bfb06fda40d4adf6763737765c678723a78
Qaz!@#wsx#@!edC1 <marcin.szymanski.poczta@gmail.com>
2019-07-11 14:19:46 +0200

    first commit

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+class Calculator {
 
-public class Calc {
+    public void adding(){
+        System.out.println("Wlasnie dodaje");
+    }
+
+    public void sub (){
+        System.out.println("Wlasnie odejmuje");
+    }
+
+    public static void main(String[] args){
+        Calculator calc = new Calculator();
+        calc.adding();
+        calc.sub();
+    }
 }
